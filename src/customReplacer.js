673949bb60627349a97ec3dc8937923530e329b3
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-11 22:08:57 +0530

    Update customReplacer.js

--- src/customReplacer.js
+++ src/customReplacer.js
@@ -13,10 +13,7 @@ const { TIMEZONE } = require("./config");
 module.exports = (template, replaceData) => {
   if (replaceData.search && replaceData.replace) {
     if (!replaceData.eval || replaceData.replace.length <= 50) {
-      const replace = replaceData?.eval
-        ? eval(replaceData.replace)
-        : replaceData.replace;
-      template = template.split(replaceData.search).join(replace);
+      template = template.split(replaceData.search).join(replaceData?.eval ? eval(replaceData.replace) : replaceData.replace);
     } else {
       return {
         result: false,
