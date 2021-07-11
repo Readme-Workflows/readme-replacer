72fb0390445fb026cd5c7d8d1ff546c4393e4b66
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-11 19:23:32 +0530

    Update customReplacer.js

--- src/customReplacer.js
+++ src/customReplacer.js
@@ -3,14 +3,16 @@
  * @param {string} template Template file content
  * @param {object} replace Replace config
  */
+const { inspect } = require("util");
 module.exports = (template, replaceData) => {
   if (replaceData.search && replaceData.replace) {
     if (!replaceData.eval || replaceData.replace.length <= 50) {
+      let replace = replaceData?.eval ? eval(replaceData.replace) : replaceData.replace;
+      if (typeof replace !== "string")
+        replace = inspect(replace, {depth:0});
       template = template
         .split(replaceData.search)
-        .join(
-          replaceData.eval ? eval(replaceData.replace) : replaceData.replace
-        );
+        .join(replace);
     } else {
       return {
         result: false,
